5fe3af090a8a1d28ad2fcf5dc2103b83bd1c7aa4
Rudy Mollet <91132260+Zenitude@users.noreply.github.com>
2021-12-27 17:01:38 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
-//Récupération du body
+// Récupération du body / Body recovery
 const body = document.body;
 
-//Création du container principal
+// Création du container principal / Creation of the main container
 const containerPrincipal = document.createElement('div');
 containerPrincipal.setAttribute('class', 'container');
 body.appendChild(containerPrincipal);
