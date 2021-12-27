2af827f65117bddbce0418c12ac36520ff8fe118
Rudy Mollet <91132260+Zenitude@users.noreply.github.com>
2021-12-27 16:53:27 +0100

    Update footer.js

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,6 +1,6 @@
-// Création du footer
+// Création du footer / Creating the footer
 let footer = document.createElement('footer');
 containerPrincipal.appendChild(footer);
 
-// Texte du footer
+// Texte du footer / Text of the footer
 footer.innerText = '\u00a9 footer';
